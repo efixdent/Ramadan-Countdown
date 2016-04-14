a6da6be98c8064ae9e65057577b06f6d15faed23
efixdent <catatanikrom@gmail.com>
2016-04-14 10:18:09 +0700

    Create ramadan1437h.js
    Javascript for Ramadan Countdown

---
+++ ramadan1437h.js
@@ -0,0 +1,4 @@
+// By catatanikrom.blogspot.com | Inspired by www.arlinadzgn.com
+var target_date=new Date("Jun 6, 2016").getTime();var days,hours,minutes,seconds;var hitungmundur=document.getElementById("hitungmundur");setInterval(function(){var current_date=new Date().getTime();var seconds_left=(target_date- current_date)/ 1000;
+days=parseInt(seconds_left/86400);seconds_left=seconds_left%86400;hours=parseInt(seconds_left/3600);seconds_left=seconds_left%3600;minutes=parseInt(seconds_left/60);seconds=parseInt(seconds_left%60);hitungmundur.innerHTML=days+" <span class=\'digital\'>days</span> "+ hours+" <span class=\'digital\'>hours</span> "
++ minutes+" <span class=\'digital\'>minutes</span> "+ seconds+" <span class=\'digital\'>seconds to The</span> <span class=\'idjudul\'>Ramadan 1437H / 2016</span>";},1000);
